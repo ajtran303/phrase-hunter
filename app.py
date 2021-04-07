be876f7fdc516a602892d8c602ed326d6cb775ed
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2021-04-07 02:52:57 -0600

    Add first working version of game to app.main

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-# Import your Game class
+from phrasehunter.game import Game
+from phrasehunter.phrase import Phrase
 
-# Create your Dunder Main statement.
+if __name__ == '__main__':
+    phrase_1 = Phrase('Hello World')
+    phrase_2 = Phrase('Hello Mother')
+    phrase_3 = Phrase('Hello Father')
+    phrase_4 = Phrase('Here I am at')
+    phrase_5 = Phrase('Camp Granada')
+    phrase_6 = Phrase('World Hello')
+    phrase_7 = Phrase('Mother Hello')
+    phrase_8 = Phrase('Father Hello')
+    phrase_9 = Phrase('I am here at')
+    phrase_10 = Phrase('Granada Camp')
+    phrases = (phrase_1, phrase_2, phrase_3, phrase_4, phrase_5,
+               phrase_6, phrase_7, phrase_8, phrase_9, phrase_10)
 
-# Inside Dunder Main:
-## Create an instance of your Game class
-## Start your game by calling the instance method that starts the game loop
+    game = Game(*phrases)
+    game.start()
