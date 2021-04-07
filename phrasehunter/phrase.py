2bd973b3e737c3de6e2dc459cdf509be8594e890
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2021-04-06 22:38:07 -0600

    Add display method to Phrase

--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,3 +1,14 @@
 class Phrase():
+
     def __init__(self, phrase):
         self.phrase = phrase
+
+    def display(self):
+        words = self.phrase.split(' ')
+        underscores = []
+
+        for word in words:
+            word = ' '.join(['_' for x in word.lower()])
+            underscores.append(word)
+
+        return '   '.join(underscores)
