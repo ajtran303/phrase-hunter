0aac1ed6ee02ca315797565b3cd9fe0e7c804892
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2021-04-07 00:24:13 -0600

    Update Phrase class to meet step 1 specs

--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,17 +1,52 @@
 class Phrase():
 
     def __init__(self, phrase):
-        self.phrase = phrase
+        self.phrase = phrase.lower()
+        self.phrase_map = self.set_phrase_map(self.phrase)
+        self.underscores = self.set_underscores(self.phrase, init=True)
+        self.guessed_letters = list()
 
     def display(self):
-        words = self.phrase.split(' ')
-        underscores = []
-
-        for word in words:
-            word = ' '.join(['_' for x in word.lower()])
-            underscores.append(word)
-
-        return '   '.join(underscores)
+        return self.underscores
 
     def check_letter(self, letter):
-        pass
+        try:
+            indices = self.phrase_map[letter]
+            characters = self.parse_characters(self.underscores)
+            for index in indices:
+                characters[index] = letter
+            characters = ''.join(characters)
+            self.underscores = self.set_underscores(characters)
+            self.guessed_letters.append(letter)
+        except KeyError:
+            pass
+
+    def check_complete(self):
+        return '_' not in self.underscores
+
+    def parse_characters(self, underscores):
+        words = []
+        for word in underscores.split('   '):
+            word = ''.join(word.split(' '))
+            words.append(word)
+        return [character for character in ' '.join(words)]
+
+    def set_phrase_map(self, letters):
+        map = {}
+        for index, letter in enumerate(letters):
+            try:
+                map[letter].append(index)
+            except KeyError:
+                map[letter] = list()
+                map[letter].append(index)
+        return map
+
+    def set_underscores(self, words, init=False):
+        underscores = []
+        for word in words.split(' '):
+            if init is True:
+                characters = ['_' for character in word]
+            else:
+                characters = [character for character in word]
+            underscores.append(' '.join(characters))
+        return '   '.join(underscores)
